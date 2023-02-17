14f8bd1d29fd8af843fc6d68194c66b353a02d59
misskukuua <p.asare@alustudent.com>
2023-02-17 01:02:59 -0800

    renamed table

--- models/user.py
+++ models/user.py
@@ -17,7 +17,7 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    __hbnb_dev_db.users__ = "users"
+    __tablename__ = "hbnb_dev_db.users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
