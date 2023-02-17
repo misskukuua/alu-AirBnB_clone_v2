828b7c0052baf8f1c5ec7e51f9ce79a319e62c05
misskukuua <p.asare@alustudent.com>
2023-02-17 00:59:37 -0800

    pep8

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
+from .base import Base
 
 
 class User(BaseModel, Base):
