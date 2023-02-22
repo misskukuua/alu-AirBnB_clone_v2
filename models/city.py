386d2a12a7ef7b3a4234934b4f444970475c9b99
misskukuua <p.asare@alustudent.com>
2023-02-22 01:06:04 -0800

    commented

--- models/city.py
+++ models/city.py
@@ -14,8 +14,8 @@ class City(BaseModel, Base):
         state_id: The state id
         name: input name
     """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
+    __tablename__ = 'cities'
+
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities")
--- models/state.py
+++ models/state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This is the state class"""
+import os
+
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
@@ -16,20 +18,11 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    cities = relationship("City", backref="state",
+                          cascade='all, delete,delete-orphan')
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ list of city o=instances with state id"""
+            all_cities = list(models.storage.all(City).values())
+            return list(filter(lambda city: (city.id == self.id), all_cities))
