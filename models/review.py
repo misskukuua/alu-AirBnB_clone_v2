03f7b03bb74924b16cfe74241cf5dbf0260efbfe
misskukuua <p.asare@alustudent.com>
2023-02-22 11:13:19 -0800

    commented 2

--- models/review.py
+++ models/review.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 """ Review module for the HBNB project """
-from sqlalchemy.orm import relationship
-
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
@@ -16,4 +14,22 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     text = Column(String(1024), nullable=False)
-    users = relationship("User", backref="user", cascade="delete")
+
+# """ Review module for the HBNB project """
+# from sqlalchemy.orm import relationship
+#
+# from models.base_model import BaseModel, Base
+# from sqlalchemy import Column, String, ForeignKey
+#
+#
+# class Review(BaseModel, Base):
+#     """ Review classto store review information
+#      place_id = ""
+#      user_id = ""
+#      text = ""
+#     """
+#     __tablename__ = "reviews"
+#     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+#     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+#     text = Column(String(1024), nullable=False)
+#     users = relationship("User", backref="user", cascade="delete")
