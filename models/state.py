02d41ae25643c44e779b8f0eae52fde4c76f0987
misskukuua <p.asare@alustudent.com>
2023-02-22 01:07:14 -0800

    added a new line

--- models/state.py
+++ models/state.py
@@ -20,6 +20,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
                           cascade='all, delete,delete-orphan')
+
     if os.getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
