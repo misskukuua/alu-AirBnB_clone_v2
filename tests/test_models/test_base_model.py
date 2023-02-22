61f34f6c9c43d4fb3720390898c34ad8b8aa7aef
misskukuua <p.asare@alustudent.com>
2023-02-22 09:47:10 -0800

    updated test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -96,13 +96,4 @@ class test_basemodel(unittest.TestCase):
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
         new = BaseModel(**n)
-        self.assertAlmostEqual(new.created_at.timestamp(),
-                               new.updated_at.timestamp(), delta=1)
-
-        # def test_updated_at(self):
-        #     """ """
-        #     new = self.value()
-        #     self.assertEqual(type(new.updated_at), datetime.datetime)
-        #     n = new.to_dict()
-        #     new = BaseModel(**n)
-        #     self.assertAlmostEqual(new.created_at.timestamp()
+        self.assertFalse(new.created_at == new.updated_at)
