c5d6be25e741820a784b94662225ee87a74bd05d
misskukuua <p.asare@alustudent.com>
2023-02-22 01:47:42 -0800

    updated review and place

--- models/review.py
+++ models/review.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
@@ -14,3 +16,4 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     text = Column(String(1024), nullable=False)
+    users = relationship("User", backref="user", cascade="delete")
