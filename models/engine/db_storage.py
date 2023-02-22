cd97a9ac0649b8483c16dcf228c6f2e041991630
misskukuua <p.asare@alustudent.com>
2023-02-22 01:15:12 -0800

    updated db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ New engine DBStorage """
+import sqlalchemy
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
@@ -25,14 +26,21 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        """ init method """
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            os.getenv('HBNB_MYSQL_USER'),
-            os.getenv('HBNB_MYSQL_PWD'),
-            os.getenv('HBNB_MYSQL_HOST'),
-            os.getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
+        """initialize object"""
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        database = os.getenv('HBNB_MYSQL_DB')
 
-        if os.getenv('HBNB_ENV') == 'test':
+        self.__engine = sqlalchemy.create_engine(
+            'mysql+mysqldb://{}:{}@{}:3306/{}'
+            .format(user,
+                    password,
+                    host,
+                    database), pool_pre_ping=True)
+
+        if os.getenv('HBNB_ENV') == "test":
+            # from models.base_model import Base
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
@@ -67,16 +75,14 @@ class DBStorage:
             self.__session.delete(obj)
             self.save()
 
-    def reload(self, remove=False):
+    def reload(self):
         """ reload method """
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         session = scoped_session(session_factory)
-        if remove:
-            session.remove()
         self.__session = session()
 
-    def close(self):
-        """ close method """
-        self.reload(remove=True)
+    # def close(self):
+    #     """ close method """
+    #     self.reload(remove=True)
