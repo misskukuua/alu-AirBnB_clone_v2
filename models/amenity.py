ab27f3a369d4569d2045adbaf008b07066672e8b
misskukuua <p.asare@alustudent.com>
2023-02-24 15:36:50 -0800

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -13,5 +13,5 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship('Place', secondary='place_amenity')
+    # if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    place_amenities = relationship('Place', secondary='place_amenity')
