68e55c4c9a8c749b174c4ffd4fffd0f8de3b5eb7
misskukuua <p.asare@alustudent.com>
2023-02-24 14:05:00 -0800

    all

--- models/conscole.py
+++ models/conscole.py
@@ -17,9 +17,8 @@ class HBNBCommand(cmd.Cmd):
     """this class is entry point of the command interpreter
     """
     prompt = "(hbnb) "
-    all_classes = {"BaseModel": BaseModel, "User": User,
-                   "State": State, "City": City,
-                   "Amenity": Amenity, "Place": Place, "Review": Review}
+    all_classes = {"BaseModel", "User", "State", "City",
+                   "Amenity", "Place", "Review"}
 
     def emptyline(self):
         """Ignores empty spaces"""
