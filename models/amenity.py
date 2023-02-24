646ed8dd054044cd425470946a4a030d5fb3a7b0
misskukuua <p.asare@alustudent.com>
2023-02-24 15:39:59 -0800

    amenitys

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ import os
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -14,4 +15,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     # if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    place_amenities = relationship('Place', secondary='place_amenity')
+    place_amenities = relationship('Place', secondary=place_amenity)
