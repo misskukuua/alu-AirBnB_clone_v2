fa192b7974efe8e53fb42039a39a3709741e4701
Mugisha-Beline <belinemugisha21@gmail.com>
2023-03-04 13:23:02 +0200

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
-# Script that configures Nginx server with some folders and files
-apt-get -y update
-apt-get -y install nginx
-service nginx start
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Africa Leadership University" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+# Create fake HTML file for testing
+echo "<html><head><title>Test Page</title></head><body><h1>This is a test page.</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test /data/web_static/current
+
+# Set ownership of directories
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config_file="/etc/nginx/sites-available/default"
+config_block="location /hbnb_static/ { alias /data/web_static/current/; }"
+if ! grep -q "$config_block" "$config_file"; then
+  sudo sed -i "/^\s*location\s*\/\s*{\s*$/a $config_block" "$config_file"
+fi
+
+# Restart Nginx
+sudo service nginx restart
+
