6030b7734078228117aec4bc0d548e788ab58b2e
Mugisha-Beline <belinemugisha21@gmail.comm>
2023-03-07 14:19:15 +0200

    prepare web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
 # Install Nginx if not already installed
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+# create folders
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+#create a fake html file
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+# create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create fake HTML file for testing
-echo "<html><head><title>Test Page</title></head><body><h1>This is a test page.</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create symbolic link
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test /data/web_static/current
-
-# Set ownership of directories
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-config_block="location /hbnb_static/ { alias /data/web_static/current/; }"
-if ! grep -q "$config_block" "$config_file"; then
-  sudo sed -i "/^\s*location\s*\/\s*{\s*$/a $config_block" "$config_file"
-fi
-
-# Restart Nginx
-sudo service nginx restart
+# give ownership
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
 
