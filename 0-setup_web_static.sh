86b238ecb5c0ae067dbaab4441b4aebcb3ee0e6a
misskukuua <106475425+misskukuua@users.noreply.github.com>
2023-03-09 15:36:05 -0800

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
-# Install Nginx if not already installed
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-
-# create folders
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-#create a fake html file
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-
-# create symbolic link to folder
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# give file permission
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+# Script that configures Nginx server with some folders and files
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
