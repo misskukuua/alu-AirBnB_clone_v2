7277701fd2b89989ff37af89b7f214c5022ff5fa
misskukuua <p.asare@alustudent.com>
2023-03-21 16:33:11 -0700

    replaced int

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -39,7 +39,7 @@ def number_template(n):
     return render_template('5-number.html', number=n)
 
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', number=n)
 
