10b3442a39d132fd2d409c8446385d9d827ff92c
misskukuua <p.asare@alustudent.com>
2023-03-23 01:11:17 -0700

    7-state

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application that displays a list of states
+and their associated cities.
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ fetching data from the storage engine"""
+    states = sorted(storage.all(State).values(),
+                    key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_session(response_or_exc):
+    """ Request to remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
