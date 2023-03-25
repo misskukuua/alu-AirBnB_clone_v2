76942d235af55421a21f63e90c1d8fce2f312125
misskukuua <p.asare@alustudent.com>
2023-03-24 18:16:16 -0700

    pep 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,6 +7,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """
@@ -15,10 +16,11 @@ def cities_by_states():
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
+
 @app.teardown_appcontext
 def teardown(excted):
     storage.close()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
