465ffebeb082566fc1a07c54563633d0fde29187
misskukuua <p.asare@alustudent.com>
2023-03-24 17:23:29 -0700

    pep 8

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,11 +8,13 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/states", strict_slashes=False)
 def states():
     """Displays an Html page with a all the states"""
     State = storage.all("State")
     return render_template("9-states.html", state=State)
+
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
@@ -22,10 +24,12 @@ def states_id(id):
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
+
 @app.teardown_appcontext
 def close_session(response_or_exc):
     """ Request to remove the current SQLAlchemy Session """
     storage.close()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
